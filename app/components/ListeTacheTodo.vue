dee36295bc83d3af31cde634d6c07da2ca68117c
sehbaoui <mouad>
2020-04-12 17:36:05 +0200

    remove console.log

--- app/components/ListeTacheTodo.vue
+++ app/components/ListeTacheTodo.vue
@@ -27,7 +27,7 @@
         data() {
             return {
                 listeTacheTodoFiltre: this.$store.state.TachesTodos,
-                niveauFiltre: 0
+                niveauFiltre: 0,
             }
         },
         methods: {
@@ -51,17 +51,11 @@
                 let nbTodosToModify = 0;
                 let nbTodosModified = 0;
 
-                TachesTodos.forEach(localTodo => console.log("local todo: " + localTodo.content));
-                console.log("--------------------------------");
-                distantTodos.forEach(distantTodo => console.log("distant todo: " + distantTodo.content));
-
                 distantTodos.forEach(distantTodo => {
                     if (TachesTodos.findIndex(localTodo => localTodo.uuid === distantTodo.uuid) === -1) {
                         nbTodosToDelete++;
                     }
                 });
-                console.log("Il y a " + nbTodosToDelete + " todos à supprimer");
-
                 TachesTodos.forEach(localTodo => {
                     if (!localTodo.uuid) {
                         nbTodosToAdd++
@@ -71,24 +65,17 @@
                         nbTodosToModify++
                     }
                 });
-                console.log("Il y a " + nbTodosToAdd + " todos à ajouter");
-                console.log("Il y a " + nbTodosToModify + " todos à modifier");
-
                 if (nbTodosToAdd === 0 && nbTodosToDelete === 0 && nbTodosToModify === 0) {
                     global.bus.$emit('syncDone');
-                    console.log("Synchro terminée");
                 } else {
                     TachesTodos.forEach(localTodo => {
                         if (!localTodo.uuid) {
                             global.axios.post(`/${this.$store.state.user.uuid}/todos/`, {content: localTodo.content})
                                 .then(response => {
                                     nbTodosAdded++;
-                                    console.log(nbTodosAdded + " todos ont été ajouté");
                                     if (nbTodosAdded === nbTodosToAdd) {
-                                        console.log("Ajout terminé");
                                         if (nbTodosDeleted === nbTodosToDelete && nbTodosModified === nbTodosToModify) {
                                             global.bus.$emit('syncDone');
-                                            console.log("Synchro terminée")
                                         }
                                     }
                                 }).catch(err => {
@@ -101,22 +88,17 @@
                         }
 
                         if (distantTodos.findIndex(distantTodo => (distantTodo.uuid === localTodo.uuid) && (distantTodo.content !== localTodo.content || distantTodo.done !== localTodo.done)) !== -1) {
-                            console.log("envoi de la todo " + localTodo.uuid);
                             global.axios.patch(`/${this.$store.state.user.uuid}/todos/${localTodo.uuid}`, {
                                 content: localTodo.content,
                                 done: localTodo.done
                             }).then(response => {
                                 nbTodosModified++;
-                                console.log(nbTodosModified + " todos ont été modifié");
                                 if (nbTodosModified === nbTodosToModify) {
-                                    console.log("Modification terminée");
                                     if (nbTodosAdded === nbTodosToAdd && nbTodosDeleted === nbTodosToDelete) {
                                         global.bus.$emit('syncDone');
-                                        console.log("Synchro terminée")
                                     }
                                 }
                             }).catch(err => {
-                                console.log("modif fail");
                                 alert({
                                     title: "Error",
                                     message: err.message,
@@ -131,12 +113,9 @@
                             global.axios.delete(`/${this.$store.state.user.uuid}/todos/${distantTodo.uuid}`)
                                 .then(response => {
                                     nbTodosDeleted++;
-                                    console.log(nbTodosDeleted + " todos ont été supprimé");
                                     if (nbTodosDeleted === nbTodosToDelete) {
-                                        console.log("Suppression terminée");
                                         if (nbTodosAdded === nbTodosToAdd && nbTodosModified === nbTodosToModify) {
                                             global.bus.$emit('syncDone');
-                                            console.log("Synchro terminée")
                                         }
                                     }
                                 }).catch(err => {
